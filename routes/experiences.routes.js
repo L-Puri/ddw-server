7b007cced7e4797fd9fb682bb861f332f1e1bba5
Miguel Sousa Machado <mikesmachado@gmail.com>
2022-06-28 14:11:18 +0100

    collections linked

--- routes/experiences.routes.js
+++ routes/experiences.routes.js
@@ -1,27 +1,30 @@
 // Experiences Routes
 const router = require("express").Router();
 const Experience = require("../models/Experiences.model");
+const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
+const User = require("../models/User.model")
 
 /* Create Entry Route (post) */
-router.post("/create-entry", async (req, res, next) => {
-  const { title, description, picture, owner } = req.body;
+router.post("/create-entry", isAuthenticated, async (req, res, next) => { 
+  const { title, description, picture } = req.body;
   console.log('SEE HERE NEW ENTRY ---->', req.body)
   try {
     const newExperience = await Experience.create({
       title: title.trim(),
       description: description.trim(),
       picture: picture.trim(),
-      author: owner.trim()
+      owner: req.payload._id
     });
-    User.updateOne({_id:owner}, {$push: {experiences: [newExperience]}})
-
+    const updatedUser = await User.findOneAndUpdate({_id: req.payload._id}, {$push: {experiences: [newExperience._id]}}, {new: true})
     res
       .status(201)
-      .json({ message: "New experience created", id: newExperience.id });
+      .json({ message: "New experience created"}); 
   } catch (error) {
+    console.log(error)
     res.status(500).json(error);
   }
 });
+
 
 // router.post("/create-entry", (req, res) => {
 //     console.log ("serverside-recieving (experience-creation): ", req.body) 
