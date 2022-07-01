e130f7d832a185e5214d4471e1c7d4ea6c87f1c4
Miguel Sousa Machado <mikesmachado@gmail.com>
2022-07-01 10:38:40 +0100

    login test 4

--- routes/experiences.routes.js
+++ routes/experiences.routes.js
@@ -25,37 +25,42 @@ router.post("/create-entry", isAuthenticated, async (req, res, next) => {
   }
 });
 
+/* get route for specifric exp*/
+
+router.get("/update-entry/:experienceId", async (req, res) => {
+  const { experienceId } = req.params;
+  console.log("made it here: ", experienceId)
+  const foundExperience = await Experience.findById(experienceId)
+  res.status(200).json(foundExperience)
+})
+
 
 /* Update Entry Route (put) */
 router.put("/update-entry/:experienceId", async (req, res, next) => {
   const { experienceId } = req.params;
-  const { title, description, picture } = req.body;
 
-  const newData = {};
+  //  if (title !== "") {
+  //   newData.title = title.trim();
+  // }
 
-  if (title !== "") {
-    newData.title = title.trim();
-  }
+  // if (description !== "") {
+  //   newData.description = description.trim();
+  // }
 
-  if (description !== "") {
-    newData.description = description.trim();
-  }
-
-  if (picture !== "") {
-    newData.picture = picture.trim();
-  }
+  // if (picture !== "") {
+  //   newData.picture = picture.trim();
+  // }
 
   try {
     const experience = await Experience.findByIdAndUpdate(
       experienceId,
-      newData
+      req.body
     );
     res.status(200).json({ message: "Experience updated", id: experience.id });
   } catch (error) {
     res.status(500).json(error);
   }
 });
-
 
 
 /* Delete Entry Route (delete) */
